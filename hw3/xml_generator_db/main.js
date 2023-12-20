7ffe9c0a1e7878bcc8f5b658e972937abdeb9464
chihyu0917 <71316753+chihyu0917@users.noreply.github.com>
2023-12-20 16:21:25 +0800

    change filename

--- hw3/xml_generator_db/main.js
+++ hw3/xml_generator_db/main.js
@@ -44,21 +44,21 @@ function viewsrc() {
     alert(document.body.innerHTML);
 }
 
-function addInstance() {
-    var formHtml = "<h2>新增專輯</h2>" +
-                "<form id='newOrderForm'>" +
-                "<label for='Singer'>Singer:</label><br>" +
-                "<input type='text' id='Singer' name='Singer'><br>" +
-                "<label for='AlbumName'>Album Name:</label><br>" +
-                "<input type='text' id='AlbumName' name='AlbumName'><br>" +
-                "<label for='AlbumPrice'>Album Price:</label><br>" +
-                "<input type='text' id='AlbumPrice' name='AlbumPrice'><br>" +
-                "<label for='AlbumImageUrl'>Album Image URL:</label><br>" +
-                "<input type='text' id='AlbumImageUrl' name='AlbumImageUrl'><br>" +
-                "<input type='button' value='新增' onclick='submitNewAlbum();'><br>" +
-                "</form>";
-    document.getElementById("xform").innerHTML = formHtml;
-}
+// function addInstance() {
+//     var formHtml = "<h2>新增專輯</h2>" +
+//                 "<form id='newOrderForm'>" +
+//                 "<label for='Singer'>Singer:</label><br>" +
+//                 "<input type='text' id='Singer' name='Singer'><br>" +
+//                 "<label for='AlbumName'>Album Name:</label><br>" +
+//                 "<input type='text' id='AlbumName' name='AlbumName'><br>" +
+//                 "<label for='AlbumPrice'>Album Price:</label><br>" +
+//                 "<input type='text' id='AlbumPrice' name='AlbumPrice'><br>" +
+//                 "<label for='AlbumImageUrl'>Album Image URL:</label><br>" +
+//                 "<input type='text' id='AlbumImageUrl' name='AlbumImageUrl'><br>" +
+//                 "<input type='button' value='新增' onclick='submitNewAlbum();'><br>" +
+//                 "</form>";
+//     document.getElementById("xform").innerHTML = formHtml;
+// }
 
 // function submitNewAlbum() {
 //     var singer = document.getElementById('Singer').value;
@@ -66,52 +66,33 @@ function addInstance() {
 //     var albumPrice = document.getElementById('AlbumPrice').value;
 //     var albumImageUrl = document.getElementById('AlbumImageUrl').value;
 
-//     var xhr = new XMLHttpRequest();
-//     xhr.open("POST", "http://127.0.0.1:5000/add_order", true);
-//     xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-//     xhr.onreadystatechange = function() {
+//     var xmlData = '<Order>' +
+//                     '<Singer>' + escapeHtml(singer) + '</Singer>' +
+//                     '<AlbumName>' + escapeHtml(albumName) + '</AlbumName>' +
+//                     '<AlbumPrice>' + escapeHtml(albumPrice) + '</AlbumPrice>' +
+//                     '<AlbumImageURL>' + escapeHtml(albumImageUrl) + '</AlbumImageURL>' +
+//                 '</Order>';
+
+//     var xhttp = new XMLHttpRequest();
+//     xhttp.open("POST", "http://127.0.0.1:5000/updateXML", true);
+//     xhttp.setRequestHeader("Content-type", "application/xml");
+//     xhttp.onreadystatechange = function() {
 //         if (this.readyState == 4 && this.status == 200) {
-//             console.log("Response:", this.responseText);
-//             // Additional actions upon successful request can be added here
+//             console.log("Response received: ", this.responseText);
+//             // Reload or update the page content as needed
 //         }
 //     };
-//     var data = "Singer=" + encodeURIComponent(Singer) + "&AlbumName=" + encodeURIComponent(AlbumName) + "&AlbumPrice=" + encodeURIComponent(AlbumPrice) + "&AlbumImageUrl=" + encodeURIComponent(AlbumImageUrl);
-//     xhr.send(data);
+//     xhttp.send("newOrder=" + encodeURIComponent(xmlData));
 // }
 
-function submitNewAlbum() {
-    var singer = document.getElementById('Singer').value;
-    var albumName = document.getElementById('AlbumName').value;
-    var albumPrice = document.getElementById('AlbumPrice').value;
-    var albumImageUrl = document.getElementById('AlbumImageUrl').value;
-
-    var xmlData = '<Order>' +
-                    '<Singer>' + escapeHtml(singer) + '</Singer>' +
-                    '<AlbumName>' + escapeHtml(albumName) + '</AlbumName>' +
-                    '<AlbumPrice>' + escapeHtml(albumPrice) + '</AlbumPrice>' +
-                    '<AlbumImageURL>' + escapeHtml(albumImageUrl) + '</AlbumImageURL>' +
-                '</Order>';
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("POST", "http://127.0.0.1:5000/updateXML", true);
-    xhttp.setRequestHeader("Content-type", "application/xml");
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            console.log("Response received: ", this.responseText);
-            // Reload or update the page content as needed
-        }
-    };
-    xhttp.send("newOrder=" + encodeURIComponent(xmlData));
-}
-
-function escapeHtml(unsafe) {
-    return unsafe
-        .replace(/&/g, "&amp;")
-        .replace(/</g, "&lt;")
-        .replace(/>/g, "&gt;")
-        .replace(/"/g, "&quot;")
-        .replace(/'/g, "&#039;");
-}
+// function escapeHtml(unsafe) {
+//     return unsafe
+//         .replace(/&/g, "&amp;")
+//         .replace(/</g, "&lt;")
+//         .replace(/>/g, "&gt;")
+//         .replace(/"/g, "&quot;")
+//         .replace(/'/g, "&#039;");
+// }
 
 
 function deleteInstance() {
