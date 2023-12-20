73cda61fcc2cddd2157e302288a6f24b94d8542e
chihyu0917 <71316753+chihyu0917@users.noreply.github.com>
2023-12-20 16:50:07 +0800

    replace json to text file

---
+++ hw3/xml_DOM/generate.py
@@ -0,0 +1,36 @@
+import xml.etree.ElementTree as ET
+
+def parse_txt_to_list(txt_path):
+    with open(txt_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    
+    orders = []
+    headers = lines[0].strip().split(',')
+    for line in lines[1:]:
+        values = line.strip().split(',')
+        order = dict(zip(headers, values))
+        orders.append(order)
+    
+    return orders
+
+def generate_xml_from_list(template_path, orders, output_path):
+    tree = ET.parse(template_path)
+    root = tree.getroot()
+    
+    root.clear()
+
+    for order_data in orders:
+        order = ET.SubElement(root, "Order")
+        for key, value in order_data.items():
+            child = ET.SubElement(order, key)
+            child.text = value
+
+    tree.write(output_path, encoding='utf-8', xml_declaration=True)
+
+# 使用範例
+template_path = 'template.xml'
+txt_path = 'database.txt'
+output_path = 'output.xml'
+
+orders = parse_txt_to_list(txt_path)
+generate_xml_from_list(template_path, orders, output_path)
