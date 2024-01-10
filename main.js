fbae3343c5c005915b4e3beba8728d6c06b1f00f
chihyu0917 <71316753+chihyu0917@users.noreply.github.com>
2024-01-10 21:54:09 +0800

    modify hover

--- main.js
+++ main.js
@@ -1,19 +1,3 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     let currentSlide = 0;
-//     const slides = document.querySelectorAll(".slide");
-//     const totalSlides = slides.length;
-
-//     // 函數：轉到下一張slide
-//     function nextSlide() {
-//         slides[currentSlide].style.display = "none";
-//         currentSlide = (currentSlide + 1) % totalSlides;
-//         slides[currentSlide].style.display = "block";
-//     }
-
-//     // 設定定時器自動轉換slide
-//     setInterval(nextSlide, 3000); // 3秒切換一次
-// });
-
 document.addEventListener("DOMContentLoaded", function () {
     let currentSlide = 0;
     const slides = document.querySelectorAll(".slide");
@@ -52,3 +36,23 @@ document.addEventListener("DOMContentLoaded", function () {
         showSlide(currentSlide + 1);
     });
 });
+
+// const slides = document.querySelectorAll('.slide');
+// const dots = document.querySelectorAll('.dot');
+
+// dots.forEach((dot, index) => {
+//     dot.addEventListener('click', () => {
+//         updateSlides(index);
+//         updateDots(index);
+//     });
+// });
+
+// function updateSlides(index) {
+//     slides.forEach(slide => slide.classList.remove('active'));
+//     slides[index].classList.add('active');
+// }
+
+// function updateDots(index) {
+//     dots.forEach(dot => dot.classList.remove('active'));
+//     dots[index].classList.add('active');
+// }
